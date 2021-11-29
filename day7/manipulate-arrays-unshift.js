9355199fb60a5a8a07872a798f0267c0dc1815df
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2021-11-29 19:49:31 +0530

    day7

---
+++ day7/manipulate-arrays-unshift.js
@@ -0,0 +1,7 @@
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+console.log(myArray.shift())
+console.log(myArray)
+// Only change code below this line
+console.log(myArray.unshift(["Paul", 35]))
+console.log(myArray)
