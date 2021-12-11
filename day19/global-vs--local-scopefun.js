438a15e0f85efb497dbbb54721f6b9532b22ba27
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2021-12-11 22:44:02 +0530

    day19

---
+++ day19/global-vs--local-scopefun.js
@@ -0,0 +1,11 @@
+// Setup
+const outerWear = "T-Shirt";
+myOutfit();
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater";
+  // Only change code above this line
+  console.log(outerWear);
+  return outerWear;
+}
+console.log(outerWear);
