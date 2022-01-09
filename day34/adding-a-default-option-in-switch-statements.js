07c4d0919349d9b339d0ccbdd65d46b70189715c
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-01-09 22:34:45 +0530

    day34

---
+++ day34/adding-a-default-option-in-switch-statements.js
@@ -0,0 +1,18 @@
+function switchOfStuff(val) {
+  switch (val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    default:
+      return "stuff";
+      break;
+  }
+}
+
+switchOfStuff(1);
