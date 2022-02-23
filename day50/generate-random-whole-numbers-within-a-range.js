3145d0f3286b578ad70cdabdc3f7a04b2ef564b4
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-02-23 20:21:10 +0530

    day50

---
+++ day50/generate-random-whole-numbers-within-a-range.js
@@ -0,0 +1,7 @@
+function randomRange(myMin, myMax) {
+  // Only change code below this line.
+  console.log(Math.floor(Math.random() * (myMax - myMin + 1)) + myMin);
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+  // Only change code above this line
+}
